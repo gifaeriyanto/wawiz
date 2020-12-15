8fa4f0be5d9fc92b27b0b9bfaa860d93dcb011a8
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-16 01:23:35 +0800

    add colors theme

---
+++ src/theme/colors.tsx
@@ -0,0 +1,25 @@
+import { ThemeOverride } from '@chakra-ui/react';
+
+const colors: ThemeOverride['colors'] = {
+  green: {
+    100: '#C9FAC8',
+    200: '#93F59A',
+    300: '#5AE171',
+    400: '#30C358',
+    500: '#009C3A',
+    600: '#00863F',
+    700: '#007040',
+    800: '#005A3C',
+    900: '#004A39',
+  },
+  red: {
+    100: '#fef2f7',
+    200: '#fcdde2',
+    300: '#fba3b1',
+    400: '#f9587a',
+    500: '#e3385c',
+    600: '#c42151',
+  },
+};
+
+export default colors;
