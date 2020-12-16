8cc916370776778b372392b00fe5a8de2cf25353
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-17 06:03:04 +0800

    routing updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,25 @@ import { ChakraProvider } from '@chakra-ui/react';
 import AuthPage from 'pages/auth';
 import DashboardPage from 'pages/dashboard';
 import React from 'react';
-import { HashRouter, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import theme from 'theme';
-import { PrivateRoute, ProvideAuth, PublicRoute } from 'utils/auth';
+import { RouteSubscribe } from 'utils/auth';
 
 const App: React.FC = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <ProvideAuth>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
         <HashRouter>
+          <RouteSubscribe />
           <Switch>
-            <PublicRoute path="/auth">
-              <AuthPage />
-            </PublicRoute>
-            <PrivateRoute path="/" exact>
-              <DashboardPage />
-            </PrivateRoute>
+            <Route path="/auth" component={AuthPage} exact />
+            <Route path="/" component={DashboardPage} exact />
+            <Route path="/dashboard">Dashboard</Route>
           </Switch>
         </HashRouter>
-      </ProvideAuth>
-    </ChakraProvider>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 };
 
