e60e6793c19b33a11b7f1074efadc199e3323b92
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-16 20:43:00 +0800

    update loadingPage

--- src/components/loadingPage/loadingPage.tsx
+++ src/components/loadingPage/loadingPage.tsx
@@ -4,7 +4,17 @@ import logo from 'static/images/logo.svg';
 
 const LoadingPage: React.FC = () => {
   return (
-    <Flex h="100vh" align="center" justify="center">
+    <Flex
+      h="100vh"
+      align="center"
+      justify="center"
+      pos="fixed"
+      top={0}
+      left={0}
+      w="100%"
+      zIndex={9999}
+      bgColor="white"
+    >
       <Box textAlign="center">
         <Image src={logo} alt="Logo Wawiz" mb={10} />
         <CircularProgress isIndeterminate color="green.400" />
