aee97986b200190cbca686508e46a4528a718188
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-18 20:49:51 +0800

    update sidebar

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -4,14 +4,17 @@ import { useEffect } from 'react';
 import { useHistory, useLocation, Redirect } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { auth } from 'utils/firebase';
+import Routes from 'utils/routes';
 
-const PUBLIC_PATH = ['/auth'];
+const PUBLIC_PATH = [Routes.auth];
 
 export const RouteSubscribe: React.FC = () => {
   const [authStatus, setAuthStatus] = useRecoilState(authState);
   const history = useHistory();
   const location = useLocation();
-  const { from } = (location.state as any) || { from: { pathname: '/' } };
+  const { from } = (location.state as any) || {
+    from: { pathname: Routes.dashboard },
+  };
 
   useEffect(() => {
     auth.onAuthStateChanged((currentState) => {
@@ -38,7 +41,7 @@ export const RouteSubscribe: React.FC = () => {
         return (
           <Redirect
             to={{
-              pathname: '/auth',
+              pathname: Routes.auth,
               state: { from: location.pathname },
             }}
           />
