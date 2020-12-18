85baf86bf004d082e2597d8cb37766c0bd09ece4
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-18 20:51:32 +0800

    add new route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import AuthPage from 'pages/auth';
+import CreateBroadcastPage from 'pages/create-broadcast';
 import DashboardPage from 'pages/dashboard';
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
@@ -16,7 +17,7 @@ const App: React.FC = () => {
           <Switch>
             <Route path="/auth" component={AuthPage} exact />
             <Route path="/" component={DashboardPage} exact />
-            <Route path="/dashboard">Dashboard</Route>
+            <Route path="/create-broadcast" component={CreateBroadcastPage} />
           </Switch>
         </HashRouter>
       </ChakraProvider>
