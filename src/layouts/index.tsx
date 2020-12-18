953b20ad72faaf2991d05ec988c43effdd982c1b
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-18 15:50:03 +0800

    move blocker to layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,33 @@
-import { Box } from '@chakra-ui/react';
-import { waState } from 'atoms/waState';
+import { Alert, AlertIcon, Box, Image, Link as CLink } from '@chakra-ui/react';
+import { waQrCode, waState, waStateFormatted } from 'atoms/waState';
+import Blocker from 'components/blocker';
 import Sidebar from 'layouts/sidebar';
 import React, { useEffect, useState } from 'react';
-import { useRecoilCallback } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 import { API } from 'utils/api';
 
 const Layouts: React.FC = ({ children }) => {
   const [isReady, setIsReady] = useState(false);
+  const waStatus = useRecoilValue(waStateFormatted).toLowerCase();
+  const qrCode = useRecoilValue(waQrCode);
+
+  const handleRefreshQrCode = useRecoilCallback(
+    ({ set }) => () => {
+      if (waStatus !== 'connected') {
+        API.get('/qr-code').then(({ data }) => {
+          set(waQrCode, data.qrCode);
+        });
+      } else {
+        set(waQrCode, '');
+      }
+    },
+    [waStatus],
+  );
+
+  useEffect(() => {
+    const intervalID = setInterval(handleRefreshQrCode, 2000);
+    return () => clearInterval(intervalID);
+  }, [handleRefreshQrCode]);
 
   useEffect(() => {
     API.get(`/start`).finally(() => {
@@ -32,10 +53,44 @@ const Layouts: React.FC = ({ children }) => {
     }
   }, [handleRefresh, isReady]);
 
+  const renderBlocker = () => {
+    if (qrCode) {
+      return (
+        <Blocker>
+          <Box textAlign="center">
+            <Image src={qrCode} w="300px" alt="QR Code" mb={8} mx="auto" />
+            <Box fontSize="lg" mb={8}>
+              Open Whatsapp in your smartphone and scan this QR code to connect!
+            </Box>
+            <Alert status="warning" borderRadius="md" maxW="300px" mx="auto">
+              <AlertIcon />
+              Need help?
+              <CLink
+                ml={2}
+                color="red.500"
+                href="https://faq.whatsapp.com/general/download-and-installation/how-to-log-in-or-out/?lang=en"
+                isExternal
+              >
+                Follow this steps
+              </CLink>
+            </Alert>
+          </Box>
+        </Blocker>
+      );
+    }
+
+    if (waStatus === 'initializing...') {
+      return <Blocker isLoading>Initializing...</Blocker>;
+    }
+
+    return null;
+  };
+
   return (
     <>
       <Sidebar />
       <Box pl="300px" bgColor="green.50" minH="100vh">
+        {renderBlocker()}
         {children}
       </Box>
     </>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,65 +2,14 @@ import { Alert, AlertIcon, Box, Flex, Image, Link } from '@chakra-ui/react';
 import { waQrCode, waStateFormatted } from 'atoms/waState';
 import Blocker from 'components/blocker';
 import Layouts from 'layouts';
-import React, { useEffect } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
+import React from 'react';
 import illustration from 'static/images/launch-wa.svg';
-import { API } from 'utils/api';
 
 const IndexPage: React.FC = () => {
-  const waStatus = useRecoilValue(waStateFormatted).toLowerCase();
-  const qrCode = useRecoilValue(waQrCode);
-
-  const handleRefreshQrCode = useRecoilCallback(
-    ({ set }) => () => {
-      if (waStatus !== 'connected') {
-        API.get('/qr-code').then(({ data }) => {
-          set(waQrCode, data.qrCode);
-        });
-      } else {
-        set(waQrCode, '');
-      }
-    },
-    [waStatus],
-  );
-
-  useEffect(() => {
-    const intervalID = setInterval(handleRefreshQrCode, 2000);
-    return () => clearInterval(intervalID);
-  }, [handleRefreshQrCode]);
-
   return (
     <Layouts>
       <Flex align="center" h="100vh" justify="center">
         <Box textAlign="center">
-          {qrCode ? (
-            <Blocker>
-              <Box textAlign="center">
-                <Image src={qrCode} w="300px" alt="QR Code" mb={8} mx="auto" />
-                <Box fontSize="lg" mb={8}>
-                  Open Whatsapp in your smartphone and scan this QR code to
-                  connect!
-                </Box>
-                <Alert
-                  status="warning"
-                  borderRadius="md"
-                  maxW="300px"
-                  mx="auto"
-                >
-                  <AlertIcon />
-                  Need help?
-                  <Link
-                    ml={2}
-                    color="red.500"
-                    href="https://faq.whatsapp.com/general/download-and-installation/how-to-log-in-or-out/?lang=en"
-                    isExternal
-                  >
-                    Follow this steps
-                  </Link>
-                </Alert>
-              </Box>
-            </Blocker>
-          ) : null}
           <Image src={illustration} alt="Launch WA" mb={8} />
         </Box>
       </Flex>
