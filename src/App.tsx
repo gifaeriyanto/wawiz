00e6ba9d88cc69627ca907b7dce3faf59fb9a9cb
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-12-20 06:03:29 +0800

    react-query integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import AuthPage from 'pages/auth';
 import CreateBroadcastPage from 'pages/create-broadcast';
 import DashboardPage from 'pages/dashboard';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import theme from 'theme';
 import { RouteSubscribe } from 'utils/auth';
 
 const App: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
     <RecoilRoot>
-      <ChakraProvider theme={theme}>
-        <HashRouter>
-          <RouteSubscribe />
-          <Switch>
-            <Route path="/auth" component={AuthPage} exact />
-            <Route path="/" component={DashboardPage} exact />
-            <Route path="/create-broadcast" component={CreateBroadcastPage} />
-          </Switch>
-        </HashRouter>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ChakraProvider theme={theme}>
+          <HashRouter>
+            <RouteSubscribe />
+            <Switch>
+              <Route path="/auth" component={AuthPage} exact />
+              <Route path="/" component={DashboardPage} exact />
+              <Route path="/create-broadcast" component={CreateBroadcastPage} />
+            </Switch>
+          </HashRouter>
+        </ChakraProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 };
